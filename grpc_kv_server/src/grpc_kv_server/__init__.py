c21d201b955aa2d2d44c1e744b2d221cd44e7fa6
Richard Belleville <rbellevi@google.com>
2019-07-10 13:28:34 -0700

    Test basic reads and writes in-process

--- grpc_kv_server/src/grpc_kv_server/__init__.py
+++ grpc_kv_server/src/grpc_kv_server/__init__.py
@@ -8,11 +8,10 @@ import grpc
 import key_value_pb2
 import key_value_pb2_grpc
 
-
 _ONE_DAY = datetime.timedelta(days=1)
 
 
-class KeyValueStore(key_value_pb2_grpc.KeyValueStoreServicer):
+class KeyValueStore:
     def __init__(self):
         self._data = {}
 
@@ -22,8 +21,25 @@ class KeyValueStore(key_value_pb2_grpc.KeyValueStoreServicer):
     def get(self, key):
         return self._data[key]
 
+
+class KeyValueStoreServer(key_value_pb2_grpc.KeyValueStoreServicer):
+    def __init__(self):
+        self._kv_store = KeyValueStore()
+
     def GetValue(self, request, context):
-        return key_value_pb2.GetResponse(value=self.get(request.key))
+        value = self._kv_store.get(request.key)
+        key_value_pair = key_value_pb2.KeyValuePair(key=request.key,
+                                                    value=value)
+        return key_value_pb2.GetResponse(key_value_pair=key_value_pair)
+
+    def StoreValue(self, request, context):
+        if not request.HasField("key_value_pair"):
+            context.abort(grpc.StatusCode.INVALID_ARGUMENT,
+                          "Request must have 'key_value_pair' field.")
+        self._kv_store.store(request.key_value_pair.key,
+                             request.key_value_pair.value)
+        return key_value_pb2.StoreResponse(
+            key_value_pair=request.key_value_pair)
 
 
 def _await_termination(server):
@@ -35,11 +51,19 @@ def _await_termination(server):
         server.stop(0)
 
 
-def run_server(host='localhost', port=50051):
+def _run_server_non_blocking(host, port):
     server = grpc.server(concurrent.futures.ThreadPoolExecutor())
-    key_value_pb2_grpc.add_KeyValueStoreServicer_to_server(KeyValueStore(), server)
+    key_value_pb2_grpc.add_KeyValueStoreServicer_to_server(
+        KeyValueStoreServer(), server)
     actual_port = server.add_insecure_port('{}:{}'.format(host, port))
     logging.info("Server listening at {}:{}".format(host, actual_port))
+    server.start()
+    return server, actual_port
+
+
+def run_server(host='localhost', port=50051):
+    _run_server_non_blocking(host, port)
     _await_termination(server)
+
 
 __all__ = [run_server, KeyValueStore]
--- grpc_kv_server/src/test/kv_server_test.py
+++ grpc_kv_server/src/test/kv_server_test.py
@@ -1,15 +1,47 @@
-from unittest import TestCase
+import unittest
+import contextlib
 
 import grpc_kv_server
 
-class TestGrpcKvServer(TestCase):
+import grpc
+import key_value_pb2
+import key_value_pb2_grpc
+
+
+@contextlib.contextmanager
+def _test_server():
+    server, port = grpc_kv_server._run_server_non_blocking('localhost', 0)
+    try:
+        yield server, port
+    finally:
+        server.stop(0)
+
+
+class TestGrpcKvServer(unittest.TestCase):
     def test_without_grpc(self):
         kv_store = grpc_kv_server.KeyValueStore()
         kv_store.store("golden-retriever", "pancakes")
         self.assertEqual("pancakes", kv_store.get("golden-retriever"))
 
     def test_with_grpc_in_a_single_process(self):
-        pass
+        with _test_server() as (server, port):
+            with grpc.insecure_channel('localhost:{}'.format(port)) as channel:
+                stub = key_value_pb2_grpc.KeyValueStoreStub(channel)
+                store_request = key_value_pb2.StoreRequest(
+                    key_value_pair=key_value_pb2.KeyValuePair(
+                        key="golden-retriever",
+                        value="pancakes",
+                    ))
+                store_response = stub.StoreValue(store_request,
+                                                 wait_for_ready=True)
+                self.assertEqual(store_response.key_value_pair,
+                                 store_request.key_value_pair)
+
+                get_request = key_value_pb2.GetRequest(key="golden-retriever")
+                get_response = stub.GetValue(get_request)
+                self.assertEqual(get_response.key_value_pair,
+                                 store_request.key_value_pair)
+
 
 if __name__ == "__main__":
     unittest.main()
