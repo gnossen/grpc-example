ec2b881a4949544961c600ba4d908e1bea1a1935
Richard Belleville <rbellevi@google.com>
2019-07-10 16:38:01 -0700

    Add explanations

--- grpc_kv_client/src/test/kv_client_test.py
+++ grpc_kv_client/src/test/kv_client_test.py
@@ -21,17 +21,21 @@ class TestGrpcKeyValueClient(unittest.TestCase):
         fake_channel = grpc_testing.channel(
             key_value_pb2.DESCRIPTOR.services_by_name.values(),
             grpc_testing.strict_real_time())
+        # Offload the request to another thread so we can use this thread to
+        # fake the server-side results.
         result_future = thread_pool.submit(
             grpc_kv_client.create,
             "localhost:1234",
             "golden-retriever",
             "pancakes",
             channel=fake_channel)
+        # Fake the server-side results.
         invocation_metadata, request, rpc = (fake_channel.take_unary_unary(
             _get_method("CreateRecord")))
         rpc.send_initial_metadata(())
         rpc.terminate(
             key_value_pb2.Record(name="golden-retriever", value="pancakes"),
             (), grpc.StatusCode.OK, "")
+        # Ensure the client had the correct response.
         result = result_future.result()
         self.assertIsNone(result)
