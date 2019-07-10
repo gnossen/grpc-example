b1b9788e0681ba9c3efa42d6b75f6fae6960adaf
Richard Belleville <rbellevi@google.com>
2019-07-09 19:28:39 -0700

    Change structure a bit

--- grpc_kv_server/setup.py
+++ grpc_kv_server/setup.py
@@ -5,14 +5,13 @@ import grpc_tools.command
 
 if __name__ == "__main__":
     setuptools.setup(
-        name="grpckvserver",
+        name="grpc-kv-server",
         install_requires=["grpcio"],
         setup_requires=["grpcio-tools"],
         author="Richard Belleville",
         author_email="rbellevi@google.com",
         description="An example usage of gRPC Python.",
-        packages=["grpc_kv_server"],
-        package_dir={"": "grpc_kv_server"},
+        package_dir={"": "grpc_kv_server", "test": "test"},
         scripts=["bin/grpc_kv_server"],
         test_suite="test",
         cmdclass={"generate": grpc_tools.command.BuildPackageProtos},
