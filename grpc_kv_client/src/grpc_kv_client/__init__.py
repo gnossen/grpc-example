305062d622b4a5ab6198c6e06c85e95973010062
Richard Belleville <j.belleville.richard@gmail.com>
2020-07-05 18:33:16 -0700

    Use simple stubs wherever possible

--- grpc_kv_client/src/grpc_kv_client/__init__.py
+++ grpc_kv_client/src/grpc_kv_client/__init__.py
@@ -1,74 +1,41 @@
 import grpc
+import grpc.experimental
 
 protos = grpc.protos("key_value.proto")
 services = grpc.services("key_value.proto")
+KeyValueStore = services.KeyValueStore
 
-def _get(channel, key):
-    stub = services.KeyValueStoreStub(channel)
-    get_request = protos.GetRecordRequest(name=key)
-    record = stub.GetRecord(get_request)
+def get(server, key):
+    record = KeyValueStore.GetRecord(
+            protos.GetRecordRequest(name=key),
+            server,
+            insecure=True)
     return record.value
 
 
-def get(server, key, channel=None):
-    if channel is not None:
-        return _get(channel, key)
-    else:
-        with grpc.insecure_channel(server) as channel:
-            return _get(channel, key)
+def create(server, key, value):
+    KeyValueStore.CreateRecord(
+        protos.CreateRecordRequest(
+            record=protos.Record(name=key, value=value)),
+        server,
+        insecure=True)
 
 
-def _create(channel, key, value):
-    stub = services.KeyValueStoreStub(channel)
-    create_request = protos.CreateRecordRequest(
-        record=protos.Record(
-            name=key,
-            value=value,
-        ))
-    record = stub.CreateRecord(create_request)
+def update(server, key, value):
+    KeyValueStore.UpdateRecord(
+            protos.UpdateRecordRequest(
+                record=protos.Record(name=key, value=value)),
+            server,
+            insecure=True)
 
 
-def create(server, key, value, channel=None):
-    if channel is not None:
-        _create(channel, key, value)
-    else:
-        with grpc.insecure_channel(server) as channel:
-            _create(channel, key, value)
-
-
-def _update(channel, key, value):
-    stub = services.KeyValueStoreStub(channel)
-    update_request = protos.UpdateRecordRequest(
-        record=protos.Record(
-            name=key,
-            value=value,
-        ))
-    update_response = stub.UpdateRecord(update_request)
-
-
-def update(server, key, value, channel=None):
-    if channel is not None:
-        _update(channel, key, value)
-    else:
-        with grpc.insecure_channel(server) as channel:
-            _update(channel, key, value)
-
-
-def _watch(channel, key):
-    stub = services.KeyValueStoreStub(channel)
-    watch_request = protos.WatchRecordRequest(name=key)
-    for record in stub.WatchRecord(watch_request):
-        yield record.value
-
-
-def watch(server, key, channel=None):
-    if channel is not None:
-        for value in _watch(channel, key):
-            yield value
-    else:
-        with grpc.insecure_channel(server) as channel:
-            for value in _watch(channel, key):
-                yield value
+def watch(server, key):
+    values = KeyValueStore.WatchRecord(
+        protos.WatchRecordRequest(name=key),
+        server,
+        insecure=True)
+    for value in values:
+        yield value
 
 
 __all__ = [get, create, update]
